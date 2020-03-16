1badd15af38a59b625da99b79a1fbbeafd913473
phuochoit <phuocck8@gmail.com>
2020-03-16 17:18:46 +0700

    first commit

--- index.php
+++ index.php
@@ -86,23 +86,6 @@ foreach ($table->find('tr') as $k => $result) {
 $results_jons = array(
     'messages' =>  $results
 );
-$aa = '{
-    "messages": [
-      {
-        "text":  "Did you enjoy the last game of the CF Rockets?",
-        "quick_replies": [
-          {
-            "title":"Loved it!",
-            "block_names": ["Block 1", "Block 2"]
-          },
-          {
-            "title":"Not really...",
-            "url": "https://rockets.chatfuel.com/api/sad-match",
-            "type":"json_plugin_url"
-          }
-        ]
-      }
-    ]
-  }';
-// print json_encode($results_jons);
-print $aa;
+
+/print json_encode($results_jons);
+
