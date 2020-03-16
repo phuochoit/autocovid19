c604a0c710c95f1b32dcca310783befd70114dcd
phuochoit <phuocck8@gmail.com>
2020-03-16 23:10:41 +0700

    first commit

--- index.php
+++ index.php
@@ -65,12 +65,12 @@ foreach ($table->find('tr') as $k => $result) {
             'sochet' =>  $result->find('td', 2)->plaintext,
             'hoiphuc' =>  $result->find('td', 3)->plaintext,
         );
-        $results_items[] = $items['ten'] . ' Số Ca Nhiễm ' . $items['soca'] . ' Số ca tử vong ' . $items['sochet'] . ' Số Ca đã xuất Viện ' . $items['hoiphuc'].'.
-        ';
+        $results_items[] = $items['ten'] . ' Số Ca Nhiễm ' . $items['soca'] . ' Số ca tử vong ' . $items['sochet'] . ' Số Ca đã xuất Viện ' . $items['hoiphuc'].'.';
     }
 }
 
-$result_total[]['text'] = implode(' ', $results_items);
+$result_total[]['text'] = implode('
+', $results_items);
 
 $results_jons = array(
     'messages' =>  $result_total
